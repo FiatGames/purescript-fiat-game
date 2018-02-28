b0c399c83a1a3d6ca58c4596c2ad7c0591f27d27
BELLARMINE\aruf <aruf@bellarmine.edu>
2018-02-28 14:52:45 -0500

    Init

---
+++ src/FiatGame/Types.purs
@@ -0,0 +1,61 @@
+module FiatGame.Types where
+
+import Prelude
+
+import Control.Monad.Eff (Eff)
+import Control.Monad.Eff.Console (CONSOLE, log)
+import Control.Monad.Except (ExceptT(..), lift, mapExceptT, runExceptT)
+import DOM (DOM)
+import DOM.HTML (window)
+import DOM.HTML.Types (HTMLElement, htmlDocumentToDocument, readHTMLElement)
+import DOM.HTML.Window (document)
+import DOM.Node.Document (createElement, getElementsByTagName)
+import DOM.Node.Element (getAttribute)
+import DOM.Node.HTMLCollection as C
+import DOM.Node.Node (insertBefore, parentElement)
+import DOM.Node.Types (elementToNode)
+import Data.Bifunctor (bimap)
+import Data.Either (Either, note)
+import Data.Foldable (find)
+import Data.Foreign (toForeign)
+import Data.Int (fromString)
+import Data.Lens ((^.))
+import Data.Newtype (unwrap)
+import Data.Tuple (Tuple(..))
+import Data.URI.AbsoluteURI (Query(..), _query, parse)
+
+type FiatGameQueryParams =
+  { webSocketUri :: String
+  , userId :: Int 
+  }
+
+getQueryParams :: String -> Either String FiatGameQueryParams
+getQueryParams href = do
+  parsed <- bimap show id $ parse href
+  (Query q) <- note "No querystring in src" $ parsed ^. _query
+  (Tuple _ msocket) <- note "No socket attribute in querystring" $ find (\(Tuple h _) -> h == "socket") q
+  socket <- note "No socket attribute in querystring" msocket
+  (Tuple _ muserId) <- note "No socket attribute in querystring" $ find (\(Tuple h _) -> h == "userId") q
+  userId <- note "Can't parse userId" (join (fromString <$> muserId))
+  pure $  
+    { webSocketUri : socket
+    , userId : userId
+    }
+
+makeWrapper :: forall e. Eff ( dom :: DOM | e) (Either String (Tuple HTMLElement FiatGameQueryParams))
+makeWrapper = do
+  htmlDoc <- window >>= document
+  let doc = htmlDocumentToDocument htmlDoc
+  scripts <- getElementsByTagName "script" doc
+  l <- C.length scripts
+  runExceptT $ do
+    me <- ExceptT $ note "Can't find my script" <$> C.item (l-1) scripts
+    src <- ExceptT $ note "Can't find src attribute" <$> getAttribute "src" me
+    queryParams <- ExceptT $ pure $ getQueryParams src
+    let currScript = elementToNode me
+    parEl <- ExceptT $ note "Can't find parent element" <$> parentElement currScript
+    n <- lift $ do
+      wrapper <- createElement "div" doc
+      insertBefore (elementToNode wrapper) currScript (elementToNode parEl)
+    el <- mapExceptT (pure <<< bimap show id <<< unwrap) $ readHTMLElement $ toForeign n
+    pure $ Tuple el queryParams
