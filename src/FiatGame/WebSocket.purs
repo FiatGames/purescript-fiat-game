d0023bc33c2d88e784238b38dbf2d6d75898d761
BELLARMINE\aruf <aruf@bellarmine.edu>
2018-05-02 16:37:21 -0400

    added new sender and receiver

--- src/FiatGame/WebSocket.purs
+++ src/FiatGame/WebSocket.purs
@@ -2,6 +2,7 @@ module FiatGame.WebSocket where
 
 import Prelude
 
+import Chat.Types as Chat
 import Control.Coroutine (Consumer)
 import Control.Coroutine as CR
 import Control.Coroutine.Aff as CRA
@@ -17,20 +18,24 @@ import DOM.Websocket.Event.EventTypes as WSET
 import DOM.Websocket.Event.MessageEvent as ME
 import DOM.Websocket.WebSocket (WebSocket)
 import DOM.Websocket.WebSocket as WS
-import Data.Argonaut.Core (stringify)
+import Data.Argonaut.Core (Json, stringify)
+import Data.Argonaut.Decode as Argonaut
+import Data.Argonaut.Decode.Class (class DecodeJson)
+import Data.Argonaut.Encode (class EncodeJson)
+import Data.Argonaut.Encode as Argonaut
 import Data.Argonaut.Generic.Aeson (decodeJson, encodeJson)
 import Data.Argonaut.Parser (jsonParser)
 import Data.Either (Either(..), either)
 import Data.Foldable (for_)
 import Data.Foreign (F, Foreign, readString, toForeign)
 import Data.Generic (class Generic)
-import Data.Maybe (Maybe(..))
+import Data.Maybe (Maybe(..), maybe)
 import Data.Tuple (Tuple(..))
-import FiatGame.Types (FiatGameHash(..), FiatPlayer(..))
 import FiatGame.ToClient.Types as ToClient
+import FiatGame.ToServer.Types (Cmd(..))
 import FiatGame.ToServer.Types as ToServer
+import FiatGame.Types (FiatGameHash(..), FiatPlayer(..), FutureMove(..), GameState(..))
 import Halogen as H
-import Chat.Types as Chat
 
 wsProducer
   :: forall eff
@@ -51,6 +56,48 @@ wsProducer socket = CRA.produce \emit ->
   readHelper read =
     either (const Nothing) Just <<< runExcept <<< read <<< toForeign
 
+wsConsumer' :: forall f a m state settings move eff b. Monad m 
+  => DecodeJson state 
+  => DecodeJson settings
+  => DecodeJson move
+  => MonadEff (exception :: EXCEPTION | eff) m
+  => Discard b 
+  => (f Unit -> m b) -> (ToClient.Msg settings state move -> Unit -> f Unit) -> Consumer String m a
+wsConsumer' query f = CR.consumer \msg -> do
+  case jsonParser msg >>= decodeJson of
+    Left err -> do
+      _ <- liftEff $ throwException $ E.error err
+      pure Nothing
+    Right (g ::ToClient.Msg String String String) -> case g of
+      (ToClient.Error {player,error}) -> do
+        query $ H.action $ f $ ToClient.Error {player,error}
+        pure Nothing
+      (ToClient.Msg {hash, settings, state}) -> do
+        case {settings':_, state':_} <$> fromJSONString settings <*> bar pGameState state of
+          Left err -> do
+            _ <- liftEff $ throwException $ E.error err
+            pure Nothing
+          Right {settings',state'} -> do
+            query $ H.action $ f $ ToClient.Msg {hash,settings: settings', state: state'}
+            pure Nothing
+
+fromJSONString :: forall a. DecodeJson a => String -> Either String a
+fromJSONString t = jsonParser t >>= Argonaut.decodeJson
+
+bar :: forall f b a. Applicative f => (a -> f b) -> Maybe a -> f (Maybe b)
+bar f s = maybe (pure Nothing) (map Just <<< f) s
+
+pFmv :: forall a. DecodeJson a => FutureMove String -> Either String (FutureMove a)
+pFmv (FutureMove mv) =  do
+  m <- fromJSONString mv._futureMoveMove
+  pure $ FutureMove mv{_futureMoveMove= m}
+
+pGameState :: forall t219 t220. DecodeJson t220 => DecodeJson t219 => GameState String String -> Either String (GameState t220 t219)
+pGameState (GameState s) = do
+  st <- fromJSONString s._gameStateState
+  fmv <- bar pFmv s._gameStateFutureMove
+  pure $ GameState s{_gameStateState= st, _gameStateFutureMove= fmv}
+      
 wsConsumer :: forall f a m state settings move eff b. Monad m 
   => Generic state 
   => Generic settings
@@ -101,3 +148,22 @@ wsSender userId socket f = CR.consumer \msg -> do
     , hash
     }
   pure Nothing
+
+wsSender' :: forall eff settings move a m msg. Monad m 
+  => MonadEff (dom :: DOM | eff ) m
+  => EncodeJson move 
+  => EncodeJson settings
+  => Int -> WebSocket -> (msg -> (Tuple FiatGameHash (ToServer.Cmd settings move))) -> Consumer msg m a
+wsSender' userId socket f = CR.consumer \msg -> do
+  let (Tuple hash cmd) = f msg
+  liftEff $ WS.sendString socket $ stringify $ encodeJson $ ToServer.Msg
+    { player: FiatPlayer userId
+    , cmd: pCmd cmd
+    , hash: hash
+    }
+  pure Nothing
+
+pCmd :: forall s m. EncodeJson s => EncodeJson m => ToServer.Cmd s m -> ToServer.Cmd String String
+pCmd StartGame = StartGame
+pCmd (UpdateSettings s) = UpdateSettings $ stringify $ Argonaut.encodeJson s
+pCmd (MakeMove mv) = MakeMove $ stringify $ Argonaut.encodeJson mv
